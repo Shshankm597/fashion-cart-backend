c4a3a18e6dfb46f5bb39322b2629225c592fbdfa
Shashank Shekhar Mishra <shshankm597@gmail.com>
2021-05-06 07:34:02 +0000

    fix: refactored some code

--- index.js
+++ index.js
@@ -37,23 +37,16 @@ app.use((req, res) => {
   res.status(404).json({ success: false, message: "route not found on server, please check"})
 })
 
-
+/**
+ * Error Handler
+ * Don't move
+ */
 app.use((err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
   }
   res.status(500).json({ error: err.message });
 });
-
-'--unhandled-rejections=strict'
-/**
- * Error Handler
- * Don't move
- */
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).json({ success: false, message: "error occured, see the errMessage key for more details", errorMessage: err.message})
-// })
 
 app.listen(PORT, () => {
   console.log('server started on port: ', PORT);
